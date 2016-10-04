b3488adde55b0223af69e4bef4a03d16ddab556e
Andrey Sokolov <sokolov.andr@gmail.com>
2016-10-04 17:13:58 +0300

    ТЗ 1

---
+++ index.js
@@ -0,0 +1,69 @@
+﻿class Pokemon {
+	constructor(name, level) {
+		this.name = name;
+		this.level = level;
+	}
+	show() {
+		console.log(this);		
+	}
+}
+
+class PokemonList extends Array {	
+	constructor(...items) {
+		super(...items);
+		console.log(this);
+	}
+	add(name, level) {
+		this.push(new Pokemon(name, level));
+		console.log(this);
+	}
+}
+
+var p = new Pokemon('Вася', 2);
+p.show();
+
+var lost = new PokemonList(new Pokemon('Вася', 2), new Pokemon('Коля', 1), new Pokemon('Миша', 3));
+
+lost.add('Васек', 5);
+lost.add('Бобр', 6);
+lost.add('Конь', 7);
+
+console.log(lost.shift());
+console.log(lost);
+
+var found = new PokemonList(new Pokemon('Лена', 12), new Pokemon('Катя', 11), new Pokemon('Оля', 13));
+
+found.add('Татьяна', 16);
+
+PokemonList.prototype.show = function() {
+	console.log(`Length: ${this.length}`);
+	
+	for (let el of this)
+		console.log(el);
+}
+
+lost.show();
+found.show();
+
+found.push(lost.pop());
+
+lost.show();
+found.show();
+
+Pokemon.prototype.valueOf = function() { 
+	return this.level;
+};
+
+PokemonList.prototype.max = function() {
+	var max = null;
+	for (let el of this)
+		if(!max || el > max) max = el;
+	return max;
+}
+
+console.log(lost.max());
+console.log(found.max());
+
+
+
+
