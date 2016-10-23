042f2c4042ebf7f28d2ec0d97f534d15a3332abf
Andrey Sokolov <sokolov.andr@gmail.com>
2016-10-23 17:49:50 +0300

    правки ДЗ 2

--- 2/hidenseek.js
+++ 2/hidenseek.js
@@ -8,45 +8,40 @@ const folders = [1,2,3,4,5,6,7,8,9,10];
 const maxToHide = 3;
 
 
-function getUnicNumbers(count, fromCount) {
-	let arr = [],
-		result = [];
-	
-	for(let i=0; i<fromCount; i++)
-		arr[arr.length] = i;
-	
-	for(let i=0; i<count; i++)
-		result[result.length] = arr.splice(random(0, arr.length-1), 1)[0];
-	
-	return result;
+function folderFormat(num) {
+	return ("0" + num).slice(-2);
 }
 
-
-fs.mkdirRecurs = function(dirPath, callback) {
-	fs.mkdir(dirPath, function(error) {
-		
-		if (error && error.code === 'ENOENT') {	  
-			fs.mkdirRecurs(path.dirname(dirPath), function(error) {
-				if(!error || error.code === 'EEXIST')
-					fs.mkdirRecurs(dirPath, callback);
-				else
-					console.log(`Ошибка создания [${dirPath}]`);
-			});			
-		}
-		else if (!error || error.code === 'EEXIST') {	  
-			callback && callback(error);
-		}
-		else
-			console.log(`Ошибка создания [${dirPath}]`);
-					
-	});
-};
-
-fs.mkdirRecursAsync = function (dirPath, toHideItems) {
-    return new Promise(function (resolve, reject) {
-        fs.mkdirRecurs(dirPath, function(err, data){
+function hidePokemonsAsync (dirPath, toHideItems) {
+	
+	let subPaths = [],
+		subPath = dirPath;
 			
-			if(toHideItems) {
+	while(subPath !== '.') {
+		subPaths.unshift(subPath);
+		
+		subPath = path.dirname(subPath);
+	}
+	
+	let iter = Promise.resolve();
+	
+	subPaths.forEach(function(path) {
+		iter = iter.then(() =>
+			new Promise(function (resolve, reject) {
+				fs.mkdir(path, function(error) {
+					if (!error || error.code === 'EEXIST')
+						resolve(path);
+					else
+						reject(error);
+				});
+			})
+		);				
+	});
+	
+	iter = iter.then(() => 		
+		new Promise(function (resolve, reject) {
+			
+			if (toHideItems) {
 				var info = '';
 				
 				toHideItems.forEach((pokemon, i) => {
@@ -56,24 +51,25 @@ fs.mkdirRecursAsync = function (dirPath, toHideItems) {
 				fs.writeFile(path.join(dirPath, 'pokemon.txt'), info, {flag: 'w'}, err => resolve(toHideItems) );
 			}
 			else
-				fs.unlink(path.join(dirPath, 'pokemon.txt'), err => resolve(toHideItems) );
-        });
-    });
-};
-
-function folderFormat(num) {
-	return ("0" + num).slice(-2);
+				fs.unlink(path.join(dirPath, 'pokemon.txt'), err => resolve(null) );
+			
+		})
+	);
+	
+	return iter;
+	
 }
+
 
 const hide = (dirPath = '', pokemonList = null, callback) => {
 	if(!pokemonList || !(pokemonList instanceof PokemonList)) return;
 	
-	let toHideCount = Math.min(maxToHide, random(1, pokemonList.length)),
+	let toHideCount = Math.min(maxToHide, random.randomNumber(1, pokemonList.length)),
 		toHideItemsByFolders = {};
 		
-	getUnicNumbers(toHideCount, pokemonList.length).forEach (
+	random.getUnicNumbers(toHideCount, pokemonList.length).forEach (
 			item => {				
-				let folder = 'f' + folderFormat(random(1, 10));
+				let folder = 'f' + folderFormat(random.randomNumber(1, 10));
 				
 				if(!toHideItemsByFolders[folder])
 					toHideItemsByFolders[folder] = [];
@@ -83,9 +79,10 @@ const hide = (dirPath = '', pokemonList = null, callback) => {
 		);
 	
 	let result = new PokemonList();
+	
 	let foldersInfo = folders.map(i => {
 		var folderName = folderFormat(i);
-		return fs.mkdirRecursAsync(path.join(dirPath, folderName), toHideItemsByFolders['f' + folderName] || null);
+		return hidePokemonsAsync(path.join(dirPath, folderName), toHideItemsByFolders['f' + folderName] || null);
 	});
 	
 	Promise.all(foldersInfo).then(function (data) {
@@ -105,18 +102,14 @@ const hide = (dirPath = '', pokemonList = null, callback) => {
 }
 
 
-fs.readFileAsync = function (filePath) {
-    return new Promise(function (resolve, reject) {
+function getPokemonsInfoFromFile(dirPath, i) {
+	let filePath = path.join(dirPath, folderFormat(i), 'pokemon.txt');	
+	
+	return new Promise(function (resolve, reject) {
         fs.readFile(filePath, {encoding: 'utf8', flag: 'r'}, function(err, data){
             resolve(!err ? data : null);				
         });
     });
-};
-
-function getPokemonsInfoFromFile(dirPath, i) {
-	let filePath = path.join(dirPath, folderFormat(i), 'pokemon.txt');
-	
-    return fs.readFileAsync(filePath);
 }
 
 
--- 2/random.js
+++ 2/random.js
@@ -1,3 +1,23 @@
-module.exports = (min, max) => {
+
+const randomNumber = (min, max) => {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const getUnicNumbers = (count, fromCount) => {
+	let arr = [],
+		result = [];
+	
+	for(let i=0; i<fromCount; i++)
+		arr[arr.length] = i;
+	
+	for(let i=0; i<count; i++)
+		result[result.length] = arr.splice(randomNumber(0, arr.length-1), 1)[0];
+	
+	return result;
+}
+
+
+module.exports = {
+	randomNumber,
+	getUnicNumbers
 }
